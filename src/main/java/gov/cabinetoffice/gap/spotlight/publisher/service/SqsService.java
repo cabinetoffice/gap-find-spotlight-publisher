9955a036bf50dd3048482c7d05340189bf718b30
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2023-12-01 16:52:58 +0000

    fix bug, optimise imports

--- src/main/java/gov/cabinetoffice/gap/spotlight/publisher/service/SqsService.java
+++ src/main/java/gov/cabinetoffice/gap/spotlight/publisher/service/SqsService.java
@@ -32,18 +32,15 @@ public class SqsService {
                     .build();
 
             ReceiveMessageResponse receiveMessageResponse;
+
+            //will get all the messages from the queue till there response from sqs contains messages
             do {
                 receiveMessageResponse = sqsClient.receiveMessage(receiveMessageRequest);
                 List<Message> messages = receiveMessageResponse.messages();
                 allMessages.addAll(messages);
 
-                // If there are more messages, adjust the next request's visibilityTimeout
-                if (!messages.isEmpty()) {
-                    receiveMessageRequest = receiveMessageRequest.toBuilder()
-                            .visibilityTimeout(0) // Set to 0 to make messages immediately visible again
-                            .build();
-                }
             } while ((receiveMessageResponse.messages().size() == 10));
+
             return allMessages;
         } catch (SqsException e) {
             logger.info("Error receiving messages from queue");
